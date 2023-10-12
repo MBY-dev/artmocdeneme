bc03a051d12cbc864ab51a4e3c2f8a86c26a50e1
Casao18 <memduhberat@gmail.com>
2023-10-12 20:01:31 +0300

    contact finish

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactUs.css";
-import pageflow7 from "./../Assets/pageflow7.png";
-import contact from "./../Assets/contactUs/Social Media/call (hidden).png"
-import Artmoc2 from "./../Assets/Artmoc Logo.png"
-import { useState } from "react";
+import contact from "./../Assets/contactUs/Social Media/call (hidden).png";
+import Artmoc2 from "./../Assets/Artmoc Logo.png";
 
 const ContactUs = () => {
   const [activeSection, setActiveSection] = useState('');
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [mailAddress, setMailAddress] = useState("");
+  const [message, setMessage] = useState("");
 
   const toggleContent = (section) => {
     if (activeSection === section) {
@@ -16,99 +18,148 @@ const ContactUs = () => {
     }
   };
 
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    if (/^[A-Za-z ]*$/.test(value)) {
+      setName(value);
+    }
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value)) {
+      setPhoneNumber(value);
+    }
+  };
+
+  const handleMailAddressChange = (e) => {
+    setMailAddress(e.target.value);
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = () => {
+    // E-posta doğrulaması ekleyin
+    if (!isValidEmail(mailAddress)) {
+      alert("Lütfen geçerli bir e-posta adresi girin.");
+      return;
+    }
+
+    // Form gönderme kodu buraya gelebilir
+    alert("Form gönderildi!");
+  };
+
   return (
     <div id="frame_contact" className="div-container-body">
-      {/* <div className="leftscroll-cu">
-        <img src={pageflow7} alt="pageflow" />
-      </div> */}
-      {/* <div className="fixedtopleft-cu">
-        <a href="#frame_art">Art</a>
-      </div>
-      <div className="fixedtopright-cu">
-        <a href="#frame_design">Design</a>
-      </div>
-      <div className="fixeddownleft-cu">
-        <a href="#frame_about">Info</a>
-      </div>
-      <div className="fixeddownright-cu">
-          <div className="social-right-down-cu">
-            <a href="/" className="contact-link">
-            <img src={contact} alt="contanct" className="contact-img" />
-            </a>
-          </div>
-        <div className="c">
-          <a href="#frame_contact">Contact</a>
-        </div>
-      </div> */}
-     <h2>Contact Us</h2>
-     <img src={contact} alt="" className="phone" />
-      <div  className="container2-position">
+      <h2>Contact Us</h2>
+      <img src={contact} alt="" className="phone" />
+      <div className="container2-position">
         <div className="container2">
-        <div className="deneme2-us">
-          <div
-            className={`section left-section ${activeSection === 'get-service' ? 'active' : ''}`}
-            onClick={() => toggleContent('get-service')}
-          >
-            <h3>Get Service</h3>
-          </div>
-          
-          <div className={`content2 ${activeSection === 'get-service' ? 'active' : ''}`}>
-            {activeSection === 'get-service' && (
-              <>
-                <div className="input-group">
-                  <input type="text" placeholder="Name" />
-                </div>
-                <div className="input-group">
-                  <input type="text" placeholder="Phone Number" />
-                </div>
-                <div className="input-group">
-                  <input type="text" placeholder="Mail Address" />
-                </div>
-                <div className="input-group">
-                  <textarea placeholder="Your Message"></textarea>
-                </div>
-                <div className="input-group">
-                  <button className="submit-button">Submit</button>
-                </div>
-              </>
-            )}
-          </div>
+          <div className="deneme2-us">
+            <div
+              className={`section left-section ${activeSection === 'get-service' ? 'active' : ''}`}
+              onClick={() => toggleContent('get-service')}
+            >
+              <h3>Get Service</h3>
+            </div>
+
+            <div className={`content2 ${activeSection === 'get-service' ? 'active' : ''}`}>
+              {activeSection === 'get-service' && (
+                <>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      value={name}
+                      onChange={handleNameChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Phone Number"
+                      value={phoneNumber}
+                      onChange={handlePhoneNumberChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Mail Address"
+                      value={mailAddress}
+                      onChange={handleMailAddressChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <textarea
+                      placeholder="Your Message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                  </div>
+                  <div className="input-group">
+                    <button className="submit-button" onClick={handleSubmit}>Submit</button>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
           <div className="deneme3-us">
-          <div
-            className={`section right-section ${activeSection === 'join-us' ? 'active' : ''}`}
-            onClick={() => toggleContent('join-us')}
-          >
-            <h3>Join Us</h3>
-          
-          </div>
-          <div className={`content2 ${activeSection === 'join-us' ? 'active' : ''}`}>
-            {activeSection === 'join-us' && (
-              <>
-                <div className="input-group">
-                  <input type="text" placeholder="Name" />
-                </div>
-                <div className="input-group">
-                  <input type="text" placeholder="Phone Number" />
-                </div>
-                <div className="input-group">
-                  <input type="text" placeholder="Mail Address" />
-                </div>
-                <div className="input-group">
-                  <textarea placeholder="Your Message"></textarea>
-                </div>
-                <div className="input-group">
-                  <button className="submit-button">Submit</button>
-                </div>
-              </>
-            )}
-          </div>
+            <div
+              className={`section right-section ${activeSection === 'join-us' ? 'active' : ''}`}
+              onClick={() => toggleContent('join-us')}
+            >
+              <h3>Join Us</h3>
+            </div>
+            <div className={`content2 ${activeSection === 'join-us' ? 'active' : ''}`}>
+              {activeSection === 'join-us' && (
+                <>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      value={name}
+                      onChange={handleNameChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Phone Number"
+                      value={phoneNumber}
+                      onChange={handlePhoneNumberChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      placeholder="Mail Address"
+                      value={mailAddress}
+                      onChange={handleMailAddressChange}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <textarea
+                      placeholder="Your Message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                  </div>
+                  <div className="input-group">
+                    <button className="submit-button" onClick={handleSubmit}>Submit</button>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
       <div>
-      <img className="artmoc-global" src={Artmoc2} alt=""  />
-    </div>
+        <img className="artmoc-global" src={Artmoc2} alt=""  />
+      </div>
     </div>
   );
 };
